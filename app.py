fc2cb60684445897c9801cd00a7796b95008ed61
John H Dellaverson <hdellaverson@gmail.com>
2024-05-15 14:37:26 -0700

    wednesday work pt 1

--- app.py
+++ app.py
@@ -12,11 +12,15 @@ def index():
 
 @app.route("/data", methods=["POST"])
 def data():
-    search = request.form["search"]
-    predictions = lstm_preds.getPrediction(
-        search
-    )  # Adjust this according to your prediction logic
-    return render_template("predPage.html", pred=json.dumps(predictions))
+    search = request.form["search"].upper()  # Ensure stock symbol is in upper case
+    predictions = lstm_preds.getPrediction(search)  # Now returns the image filename
+    print(predictions[0][0])
+    return render_template(
+        "predPage.html",
+        pred=(predictions[0][0]),
+        image_path=f"static/{search}.png",
+        stock_name=search,
+    )
 
 
 if __name__ == "__main__":
--- package/lstm_preds.py
+++ package/lstm_preds.py
@@ -39,10 +39,59 @@ def getPrediction(predictStock):
 
     # Now to do a specific prediction
     stock_quote = pdr.get_data_yahoo(
-        predictStock, start="2024-01-01", end=datetime.now()
+        predictStock, start="2023-01-01", end=datetime.now()
     )
-    new_df = stock_quote.filter(["Close"])
-    last_60_days = new_df[-60:].values
+    new_data = stock_quote.filter(["Close"])
+    new_dataset = new_data.values
+    training_data_len = int(np.ceil(len(new_dataset) * 0.95))
+    scaled_data = scaler.fit_transform(new_dataset)
+    train_data = scaled_data[0 : int(training_data_len), :]
+    # Split the data into x_train and y_train data sets
+    x_train = []
+    y_train = []
+
+    for i in range(60, len(train_data)):
+        x_train.append(train_data[i - 60 : i, 0])
+        y_train.append(train_data[i, 0])
+        if i <= 61:
+            print("funny business")
+
+    # Convert the x_train and y_train to numpy arrays
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    test_data = scaled_data[training_data_len - 60 :, :]
+    # Create the data sets x_test and y_test
+    x_test = []
+    y_test = new_dataset[training_data_len:, :]
+    for i in range(60, len(test_data)):
+        x_test.append(test_data[i - 60 : i, 0])
+
+    # Convert the data to a numpy array
+    x_test = np.array(x_test)
+
+    # Reshape the data
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    # Get the models predicted price values
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
+
+    # Plot the data
+    train = new_data[:training_data_len]
+    valid = new_data[training_data_len:]
+    valid["Predictions"] = predictions
+    # Visualize the data
+    plt.figure(figsize=(16, 6))
+    plt.title("Model")
+    plt.xlabel("Date", fontsize=18)
+    plt.ylabel("Close Price USD ($)", fontsize=18)
+    plt.plot(train["Close"])
+    plt.plot(valid[["Close", "Predictions"]])
+    plt.legend(["Train", "Val", "Predictions"], loc="lower right")
+    plt.savefig(f"static/{predictStock}.png")
+
+    # Now to do a specific prediction
+    last_60_days = new_data[-60:].values
     # Scale the data to be values between 0
     last_60_days_scaled = scaler.transform(last_60_days)
 
@@ -64,17 +113,5 @@ def getPrediction(predictStock):
     return pred_price
 
 
-# a = data.tail(1)["Close"]
-# time_match = a.index[0]
-# nextPred = pd.Series(
-#     predictions[-1],
-#     index=[time_match],
-#     name="pred_Close",
-# )
-
-# print(nextPred)
-# todayPred = pd.concat([a, nextPred], axis=1)
-# print("potat2")
-# print((todayPred))
-
-# print(len(predictions))
+if __name__ == "__main__":
+    print(getPrediction("AAPL"))
